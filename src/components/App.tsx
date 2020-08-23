212ef8a6e169b3cfabb5e1404a7c57a58c3e9988
tan-yuki <a0673040@gmail.com>
2020-08-23 11:54:01 +0900

    Formを別componentに切り出し

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,9 @@
-import React, { useState, ChangeEvent, MouseEvent, useCallback } from "react";
+import React, { useState } from "react";
 import { createAgoraClient } from "../services/createClient";
 import { LiveScreen } from "./LiveScreen";
 import { ClientRole, IAgoraRTCRemoteUser } from "agora-rtc-sdk-ng";
 import { useRemoteUsers } from "../hooks/useRemoteUsers";
+import { InitialSettingForm } from "./InitialSettingForm";
 
 const client = createAgoraClient();
 
@@ -13,33 +14,6 @@ export function App() {
   const [clientRole, setClientRole] = useState<ClientRole>("host");
   const [remoteUsers, setRemoteUsers] = useRemoteUsers(client);
   const [alreadyJoined, setJoinState] = useState(false);
-
-  const onChangeAppId = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setAppId(e.target.value);
-  }, []);
-
-  const onChangeChannelName = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      setChannelName(e.target.value);
-    },
-    []
-  );
-
-  const onChangeRole = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setClientRole(e.target.value === "host" ? "host" : "audience");
-  }, []);
-
-  const startLive = useCallback((e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setIsStarted(true);
-  }, []);
-
-  const closeLive = useCallback((e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setIsStarted(false);
-  }, []);
 
   const userIdList = [client.uid]
     .concat(
@@ -77,54 +51,16 @@ export function App() {
   return (
     <div>
       <div>
-        <form>
-          <div>
-            <label>App Id: </label>
-            <input
-              type="text"
-              value={appId}
-              onChange={onChangeAppId}
-              disabled={isStarted}
-            />
-            <label>Channel Name: </label>
-            <input
-              type="text"
-              value={channelName}
-              onChange={onChangeChannelName}
-              disabled={isStarted}
-            />
-          </div>
-          <div>
-            <span>Client Role: </span>
-            <label>
-              <input
-                type="radio"
-                name="role"
-                value="host"
-                checked={clientRole === "host"}
-                onChange={onChangeRole}
-                disabled={isStarted}
-              />
-              Host
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="role"
-                value="audience"
-                checked={clientRole === "audience"}
-                onChange={onChangeRole}
-                disabled={isStarted}
-              />
-              Audience
-            </label>
-          </div>
-          {isStarted ? (
-            <button onClick={closeLive}>Close</button>
-          ) : (
-            <button onClick={startLive}>Open</button>
-          )}
-        </form>
+        <InitialSettingForm
+          isStarted={isStarted}
+          setIsStarted={setIsStarted}
+          appId={appId}
+          setAppId={setAppId}
+          channelName={channelName}
+          setChannelName={setChannelName}
+          clientRole={clientRole}
+          setClientRole={setClientRole}
+        />
       </div>
       <div>{UserListComponent}</div>
       <div>{LiveScreenComponent}</div>
---
+++ src/components/InitialSettingForm.tsx
@@ -0,0 +1,109 @@
+import React, { useCallback, ChangeEvent, MouseEvent } from "react";
+import { ClientRole } from "agora-rtc-sdk-ng";
+
+export interface InitialSettingForm {
+  isStarted: boolean;
+  setIsStarted: (isStarted: boolean) => void;
+  appId: string;
+  setAppId: (appId: string) => void;
+  channelName: string;
+  setChannelName: (channelName: string) => void;
+  clientRole: ClientRole;
+  setClientRole: (clientRole: ClientRole) => void;
+}
+
+export function InitialSettingForm(props: InitialSettingForm) {
+  const {
+    isStarted,
+    setIsStarted,
+    appId,
+    setAppId,
+    channelName,
+    setChannelName,
+    clientRole,
+    setClientRole,
+  } = props;
+
+  const onChangeAppId = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setAppId(e.target.value);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onChangeChannelName = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+      setChannelName(e.target.value);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  const onChangeRole = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setClientRole(e.target.value === "host" ? "host" : "audience");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const startLive = useCallback((e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsStarted(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const closeLive = useCallback((e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsStarted(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <form>
+      <div>
+        <label>App Id: </label>
+        <input
+          type="text"
+          value={appId}
+          onChange={onChangeAppId}
+          disabled={isStarted}
+        />
+        <label>Channel Name: </label>
+        <input
+          type="text"
+          value={channelName}
+          onChange={onChangeChannelName}
+          disabled={isStarted}
+        />
+      </div>
+      <div>
+        <span>Client Role: </span>
+        <label>
+          <input
+            type="radio"
+            name="role"
+            value="host"
+            checked={clientRole === "host"}
+            onChange={onChangeRole}
+            disabled={isStarted}
+          />
+          Host
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="role"
+            value="audience"
+            checked={clientRole === "audience"}
+            onChange={onChangeRole}
+            disabled={isStarted}
+          />
+          Audience
+        </label>
+      </div>
+      {isStarted ? (
+        <button onClick={closeLive}>Close</button>
+      ) : (
+        <button onClick={startLive}>Open</button>
+      )}
+    </form>
+  );
+}
